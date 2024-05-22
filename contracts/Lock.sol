244f22bd896d8675601dda86ba44d2b3f83f66e6
jayalwayscodes <johnsonjardocs@gmail.com>
2024-05-22 18:02:22 +0100

    initialized hardhat

---
+++ contracts/Lock.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Voting {
+    struct Candidate {
+        string name;
+        uint256 voteCount;
+    }
+
+    Candidate[] public candidates;
+    address owner;
+    mapping(address => bool) public voters;
+
+    uint256 public votingStart;
+    uint256 public votingEnd;
+
+    constructor(string[] memory _candidateNames, uint256 _durationInMinutes) {
+        for (uint256 i =0; i < _candidateNames.length; i++){
+            candidates.push(Candidate({
+                name: _candidateNames[i],
+                voteCount: 0
+            }));
+        }
+        owner = msg.sender;
+        votingStart = block.timestamp;
+        votingEnd = block.timestamp + (_durationInMinutes * 1 minutes);
+    }
+
+    modifier onlyOwner{
+        require(msg.sender == owner);
+        _;
+    }
+}
